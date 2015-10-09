6a142667603a6a80ac73d5692b6ffc8b7bf414c8
Mike Taulty <mtaulty@hotmail.com>
2015-10-09 08:59:52 +0100

    initial attempt to add web job project

---
+++ BandOnTheRunEventHubMonitor/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BandOnTheRunEventHubMonitor
+{
+  using Microsoft.ApplicationInsights;
+  using Microsoft.ApplicationInsights.DataContracts;
+  using Microsoft.ServiceBus;
+  using System;
+  using System.Configuration;
+
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello, this is a trace");
+
+      try
+      {
+        var eventHubString =
+          ConfigurationManager.ConnectionStrings["EventHubConnection"];
+
+        var eventHubName =
+          ConfigurationManager.ConnectionStrings["EventHubName"];
+
+        var appInsightsKey =
+          ConfigurationManager.ConnectionStrings["AppInsightsKey"];
+
+        var nsm = NamespaceManager.CreateFromConnectionString(eventHubString.ConnectionString);
+
+        var hub = nsm.GetEventHub(eventHubName.ConnectionString);
+
+        TelemetryClient appInsights = new TelemetryClient();
+        appInsights.InstrumentationKey = appInsightsKey.ConnectionString;
+        var telemetryData = new EventTelemetry()
+        {
+          Name = "Event Hub Heartbeat",
+        };
+        telemetryData.Properties.Add("Status", hub.Status.ToString());
+        appInsights.TrackEvent(telemetryData);
+        appInsights.Flush();
+
+        Console.Write($"Service status is at {hub.Status}");
+      }
+      catch
+      {
+        Console.WriteLine("Catch handler");
+      }
+
+      Console.WriteLine("We're done");
+    }
+  }
+}

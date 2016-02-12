10f6f0ee97d92fb28cfd891fb4fc91ff637852ad
thebeebs <martinbeeby@hotmail.com>
2016-02-12 00:18:15 +0000

    Adding SignalR Work

---
+++ BandOnTheRunSignalRWebJob/SignalRWebJob/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Microsoft.AspNet.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace SignalRWebJob
+{
+    internal class Program
+    {       
+        private static void Main(string[] args)
+        {
+            //Set connection
+            var connection = new HubConnection("http://localhost:55815/");
+            //Make proxy to hub based on hub name on server
+            var myHub = connection.CreateHubProxy("OnTheRun");
+            //Start connection
+
+            connection.Start().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            int i = 0;
+            while (i < 10000)
+            {
+                Random random = new Random();
+                var r = random.Next(1, 200);
+
+                myHub.Invoke<string>("Send", "Band1038478478", r.ToString()).ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("There was an error calling send: {0}",
+                                          task.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Message Sent");
+                    }
+                }).Wait();
+                Task.Delay(4000).Wait();
+                i++;
+            }
+             
+
+            Console.Read();
+            connection.Stop();
+        }
+    }
+}
---
+++ BandOnTheRunSignalRWebsite/BandOnTheRunSignalRWebsite/App_Code/ontherun.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+public class OnTheRun : Hub
+{
+    static List<Tuple<string, string>> data;
+
+    public OnTheRun()
+    {
+
+        if (data == null)
+        {
+            data = new List<Tuple<string, string>>();
+        }
+    }
+    public void Logon()
+    {
+        //Clients.Caller.loadHistory(data);
+    }
+
+    public void Send(string bandName, string rate)
+    { 
+       Clients.All.broadcastMessage(rate);
+    }
+}

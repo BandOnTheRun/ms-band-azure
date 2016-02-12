586dcd9305e3ff9f87adde334e5eaf91d09956e5
Amy Kate Nicholson <amykate_7@live.com>
2016-02-12 10:47:28 +0000

    Adding sample code for Service Bus/Web job connections

---
+++ BasicSamples/BlobOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace BlobOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            CreateDemoData();
+
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        private static void CreateDemoData()
+        {
+            string connectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.Storage);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("input");
+            container.CreateIfNotExists();
+
+            CloudBlockBlob blob = container.GetBlockBlobReference("BlobOperations.txt");
+            blob.UploadText("Hello world!");
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("persons");
+            queue.CreateIfNotExists();
+
+            Person person = new Person()
+            {
+                Name = "Mathew",
+                Age = 39
+            };
+
+            queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(person)));
+        }
+    }
+}
---
+++ BasicSamples/TableOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace TableOperations
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            CreateDemoData();
+
+            JobHost host = new JobHost();
+            Task callTask = host.CallAsync(typeof(Functions).GetMethod("ManualTrigger"));
+            
+            Console.WriteLine("Waiting for async operation...");
+            callTask.Wait();
+            Console.WriteLine("Task completed: " + callTask.Status);
+
+            host.RunAndBlock();
+        }
+
+        private static void CreateDemoData()
+        {
+            string connectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.Storage);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("textinput");
+            queue.CreateIfNotExists();
+
+            queue.AddMessage(new CloudQueueMessage("Hello world!"));
+        }
+    }
+}

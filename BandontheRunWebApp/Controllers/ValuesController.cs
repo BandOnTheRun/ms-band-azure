6ba6235fbc2fd96a690bcd8e8541621d1bdfe4dd
davidgristwood <davidgri.ms@hotmail.com>
2016-06-22 16:11:39 +0100

    remove old event hub references

--- BandontheRunWebApp/Controllers/ValuesController.cs
+++ BandontheRunWebApp/Controllers/ValuesController.cs
@@ -58,34 +58,6 @@ namespace BandontheRunWebApp.Controllers
 
 
 
-        // create a SAS token
-        [Route("api/GetSasToken/{deviceId}")]
-        [HttpGet]
-        public string GetSasToken(string deviceId)
-        {
-
-            string ns = WebApiApplication.ehWebConsumerGroup.eventHubNamespace;
-            string hubName = WebApiApplication.ehWebConsumerGroup.eventHubPath;
-            string keyName = WebApiApplication.ehWebConsumerGroup.SendKeyName;
-            string key = WebApiApplication.ehWebConsumerGroup.SendKeyValue;
-
-            int TTLmins = 60 * 24;
-
-            if (deviceId == "")
-                return "";
-
-
-             TimeSpan ttl = new TimeSpan(0, TTLmins, 0);
-
-            var sas = CreateForHttpSender(keyName, key, ns, hubName, deviceId, ttl);
-
-            // add app insight
-            var tc = new Microsoft.ApplicationInsights.TelemetryClient();
-            tc.TrackEvent("SasToken dispensed");
-            
-            return sas;
-        }
-
 
         // list out known devices that we manage 
         [Route("api/GetDevicesInfo")]
--- BandontheRunWebApp/Global.asax.cs
+++ BandontheRunWebApp/Global.asax.cs
@@ -18,10 +18,6 @@ namespace BandontheRunWebApp
 
     public struct EventHubConfigInfo
     {
-        public string eventHubNamespace { get; set; }
-        public string eventHubPath { get; set; }
-        public string SendKeyName { get; set; }
-        public string SendKeyValue { get; set; }
         public string consumerGroup { get; set; }
         public string eventHubConnectionString { get; set; }
         public string storageConnectionString { get; set; } 
@@ -50,45 +46,18 @@ namespace BandontheRunWebApp
 
             // set up EventProcessorHost consumer group
 
-            GetEventHubConsumerGroupConfig(ref ehWebConsumerGroup);
-            CreateEventProcessorHostClient(ref ehWebConsumerGroup);
+//           GetEventHubConsumerGroupConfig(ref ehWebConsumerGroup);
+//           CreateEventProcessorHostClient(ref ehWebConsumerGroup);
         }
 
 
-        // create the EventProcessorHost consumer group that we will listen to data from our event hub
-        private void CreateEventProcessorHostClient(ref EventHubConfigInfo eventHubSettings)
-        {
-            try
-            {
-                // we use the EventProcessorHost class to processes data from the Event Hub
-                
-                eventHubSettings.processorHost = new EventProcessorHost(
-                        this.Server.MachineName,
-                        eventHubSettings.eventHubPath,
-                        eventHubSettings.consumerGroup.ToLowerInvariant(),
-                        eventHubSettings.eventHubConnectionString,
-                        eventHubSettings.storageConnectionString);
-                
-                // could now add eventHubSettings.processorHostOptions if needed
-                
-                // wire up our class to handle events from event hub
-                eventHubSettings.processorHost.RegisterEventProcessorAsync<WebsiteEventProcessor>().Wait();
-            }
-            catch (Exception e)
-            {
-                Debug.Print("Error happened while trying to connect Event Hub - {0}", e.Message);
-            }
-        }
+
 
 
 
         // read the BandontheRun.XXX related config from Azure portal or web.config
         private void GetEventHubConsumerGroupConfig(ref EventHubConfigInfo eventHubSettings)
         {
-            eventHubSettings.eventHubNamespace = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.eventHubNamespace"];
-            eventHubSettings.eventHubPath = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.eventHubPath"];
-            eventHubSettings.SendKeyName = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.SendKeyName"];
-            eventHubSettings.SendKeyValue = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.SendKeyValue"];
             eventHubSettings.consumerGroup = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.consumerGroup"];
             eventHubSettings.eventHubConnectionString = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.eventHubConnectionString"];
             eventHubSettings.storageConnectionString = System.Configuration.ConfigurationManager.AppSettings["BandontheRun.storageConnectionString"];
